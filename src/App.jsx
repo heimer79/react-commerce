2860894137b08b68c9971575a369f301cd076dda
Heimer Martinez <hmartinez@cafetosoftware.com>
2024-07-20 15:07:08 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-orange-700'>
+        Hola mundo desde React
+      </div>
+
+    </>
+  )
+}
+
+export default App
